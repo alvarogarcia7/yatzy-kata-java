fbfe5503085c96504b0682d6f7b0425758503a72
Alvaro Garcia <derecerca@gmail.com>
2016-05-01 21:09:11 +0200

    green

--- src/main/java/com/dodevjutsu/kata/yatzee/test/Yatzee.java
+++ src/main/java/com/dodevjutsu/kata/yatzee/test/Yatzee.java
@@ -1,10 +1,13 @@
 package com.dodevjutsu.kata.yatzee.test;
 
 public class Yatzee {
+    private final Console console;
+
     public Yatzee(Console console) {
+        this.console = console;
     }
 
     public void play() {
-
+        console.print("Category: Ones");
     }
 }
