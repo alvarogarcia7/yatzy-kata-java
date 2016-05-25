6ceca8f36905920ed049d390d19ab034f55d8f07
alvaro garcia <derecerca@gmail.com>
2016-05-25 15:48:58 +0200

    green

--- src/test/java/com/dodevjutsu/kata/yatzee/CliYatzi.java
+++ src/test/java/com/dodevjutsu/kata/yatzee/CliYatzi.java
@@ -3,14 +3,17 @@ package com.dodevjutsu.kata.yatzee;
 public class CliYatzi {
     private final Console console;
     private final UserInput userInput;
+    private final DiceRoller diceRoller;
 
     public CliYatzi (final Console console, final UserInput userInput, final DiceRoller diceRoller) {
         this.console = console;
         this.userInput = userInput;
+        this.diceRoller = diceRoller;
     }
 
     public void run () {
         console.print("Category: Ones");
+        diceRoller.rollAll();
         console.print("Dice: D1:2 D2:4 D3:1 D4:6 D5:1");
         console.print("[1] Dice to re-run:");
         userInput.read();
