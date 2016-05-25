8375592eb2ae5ee367764cfee3d3fbc6d8c53b20
alvaro garcia <derecerca@gmail.com>
2016-05-25 15:36:07 +0200

    red: ask the user to rerun dice

--- src/test/java/com/dodevjutsu/kata/yatzee/test/AcceptanceTest.java
+++ src/test/java/com/dodevjutsu/kata/yatzee/test/AcceptanceTest.java
@@ -25,6 +25,7 @@ public class AcceptanceTest {
         context.checking(new Expectations() {{
             oneOf(console).print("Category: Ones");
             oneOf(console).print("Dice: D1:2 D2:4 D3:1 D4:6 D5:1");
+            oneOf(console).print("[1] Dice to re-run:");
         }});
 
         cli.run();
