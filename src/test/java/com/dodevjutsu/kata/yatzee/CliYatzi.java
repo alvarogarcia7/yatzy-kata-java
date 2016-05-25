89e5e56b3324a7593555c1ff43aac1a35803ea47
alvaro garcia <derecerca@gmail.com>
2016-05-25 16:04:06 +0200

    refactor: remove fake it
    merge the dice with the first rerun

--- src/test/java/com/dodevjutsu/kata/yatzee/CliYatzi.java
+++ src/test/java/com/dodevjutsu/kata/yatzee/CliYatzi.java
@@ -1,6 +1,9 @@
 package com.dodevjutsu.kata.yatzee;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class CliYatzi {
     private final Console console;
@@ -15,16 +18,33 @@ public class CliYatzi {
 
     public void run () {
         console.print("Category: Ones");
-        final List<Integer> dice = diceRoller.rollAll();
+        List<Integer> dice = diceRoller.rollAll();
         console.print(String.format("Dice: D1:%d D2:%d D3:%d D4:%d D5:%d", dice.toArray(new Integer[0])));
         console.print("[1] Dice to re-run:");
-        final int count = count(userInput.read());
+        final String firstReRunRepresentation = userInput.read();
+        final int count = count(firstReRunRepresentation);
         final List<Integer> firstReRun = diceRoller.roll(count);
-        console.print("Dice: D1:1 D2:5 D3:1 D4:2 D5:1");
+        dice = merge(dice, firstReRunRepresentation, firstReRun);
+        console.print(String.format("Dice: D1:%d D2:%d D3:%d D4:%d D5:%d", dice.toArray(new Integer[0])));
         final int secondCount = count(userInput.read());
         final List<Integer> secondReRun = diceRoller.roll(secondCount);
         console.print("Dice: D1:1 D2:1 D3:1 D4:5 D5:1");
         console.print("Category Ones score: 4");
+    }
+
+    private List<Integer> merge (List<Integer> dice, final String userInput, final List<Integer> firstReRun) {
+        final List<Integer> indicesToReRun = new ArrayList<>(Stream.of(userInput.split(" ")).map(x -> x.substring(1)).map
+                (Integer::valueOf).map(x->x-1).collect(Collectors.toList()));
+        System.out.println(indicesToReRun);
+        dice = new ArrayList<>(dice);
+
+        int i = 0;
+        for (Integer integer : indicesToReRun) {
+            dice.set(integer, firstReRun.get(i));
+            i++;
+        }
+
+        return dice;
     }
 
     private int count (final String reRunDice) {
