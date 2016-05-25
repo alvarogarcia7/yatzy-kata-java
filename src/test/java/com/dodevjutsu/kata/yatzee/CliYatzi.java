088686b96c64a633def044aa21837e5310de98a0
alvaro garcia <derecerca@gmail.com>
2016-05-25 15:36:18 +0200

    green

--- src/test/java/com/dodevjutsu/kata/yatzee/CliYatzi.java
+++ src/test/java/com/dodevjutsu/kata/yatzee/CliYatzi.java
@@ -10,5 +10,6 @@ public class CliYatzi {
     public void run () {
         console.print("Category: Ones");
         console.print("Dice: D1:2 D2:4 D3:1 D4:6 D5:1");
+        console.print("[1] Dice to re-run:");
     }
 }
