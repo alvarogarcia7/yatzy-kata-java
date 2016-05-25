fbfe006dae6afdc625b3a7f44ab0351b630ce974
alvaro garcia <derecerca@gmail.com>
2016-05-25 15:45:26 +0200

    red: print the third and last dice run

--- src/test/java/com/dodevjutsu/kata/yatzee/test/AcceptanceTest.java
+++ src/test/java/com/dodevjutsu/kata/yatzee/test/AcceptanceTest.java
@@ -26,8 +26,7 @@ public class AcceptanceTest {
     public void happy_path () {
         cli = new CliYatzi(console, userInput);
 
-        context.checking(new Expectations() {
-            {
+        context.checking(new Expectations() {{
             oneOf(console).print("Category: Ones");
             oneOf(console).print("Dice: D1:2 D2:4 D3:1 D4:6 D5:1");
             oneOf(console).print("[1] Dice to re-run:");
@@ -36,6 +35,8 @@ public class AcceptanceTest {
             oneOf(console).print("Dice: D1:1 D2:5 D3:1 D4:2 D5:1");
 
             oneOf(userInput).read(); will(returnValue("D2 D4"));
+            oneOf(console).print("Dice: D1:1 D2:1 D3:1 D4:5 D5:1");
+
         }});
 
         cli.run();
